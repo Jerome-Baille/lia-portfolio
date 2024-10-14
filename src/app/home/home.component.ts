948472e35f91d8d80681a42c2dda09fc73bd8084
Jerome-Baille <baille.jerome@gmail.com>
2024-10-15 00:31:07 +0200

    Add dynamic project overview rendering with input properties and styling updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
 import { ProjectOverviewComponent } from "../project-overview/project-overview.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent, ProjectOverviewComponent],
+  imports: [CommonModule, AboutComponent, ProjectOverviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  projects = [
+    {
+      image: 'project-1.webp',
+      position: 'Program Management | Team Leader',
+      title: 'Siemens Energy - Prime Opportunity & E2A Accelerator',
+      description: 'Integration of two solutions/products with the rest of the Sales Department Projects.',
+      action: 'View Project'
+    },
+    {
+      image: 'project-2.webp',
+      position: 'Project Management ! Team Leader',
+      title: `Implementing IGT's Retailer Vue Gaming Terminals in Portugal - SCML Project`,
+      description: 'My work was focused on cross-communications between both companies.',
+      action: 'View Project'
+    },
+    {
+      image: 'project-3.webp',
+      position: 'European Project Management',
+      title: 'FIREPOCTEP  #wildfires',
+      description: 'New houses need new living style and design, we help them.',
+      action: 'View Case Study'
+    },
+    {
+      image: 'project-4.webp',
+      position: 'Cultural Management | Exhibitions Management',
+      title: 'Ki Culture',
+      description: 'I have redesigned the UI style of entire company pages.',
+      action: 'View Case Study'
+    },
+    {
+      image: 'project-5.webp',
+      position: 'UI Design',
+      title: 'Fundação Gulbenkian',
+      description: 'I have redesigned the UI style of entire company pages.',
+      action: 'View Case Study'
+    }
+  ];
 }
